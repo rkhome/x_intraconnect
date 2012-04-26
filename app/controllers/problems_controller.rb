f32301e65b768be3148b26f32585de7c76f45842
lktiwari <parasharragh@gmail.com>
2012-04-26 23:45:59 +0530

    Problem complete start solution

---
+++ app/controllers/problems_controller.rb
@@ -0,0 +1,22 @@
+class ProblemsController < ApplicationController
+
+	def new
+	end
+
+	def create
+	
+		title = params[:problem][:title]
+		description = params[:problem][:description]
+		problem = Problem.new()
+		problem.title = title
+		problem.description = description
+		problem.user_id = current_user.id	
+		if(problem.save())
+			flash[:notice] = "Successful posted"
+			redirect_to new_problem_path
+		else
+			flash[:error] = "Can't post, Try again!"
+			render :controller=>"problems", :action=>"new"
+		end
+	end
+end
